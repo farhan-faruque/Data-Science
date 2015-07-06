8c1968d430153deb4eee99883708ed1db13341af
Farhan Faruque <russel.cse05@gmail.com>
2015-07-06 14:24:54 +0600

    change config file

--- book_python_for_data_analysis/movie_lens/movie_lens.py
+++ book_python_for_data_analysis/movie_lens/movie_lens.py
@@ -5,7 +5,6 @@ users = pd.read_table('../data/movielens/users.dat',sep='::', header=None, names
 
 rnames = ['user_id', 'movie_id', 'rating', 'timestamp']
 ratings = pd.read_table('../data/movielens/ratings.dat', sep='::', header=None, names=rnames)
-#print(ratings)
 
 mnames = ['movie_id', 'title', 'genres']
 movies = pd.read_table('../data/movielens/movies.dat', sep='::', header=None, names=mnames)
